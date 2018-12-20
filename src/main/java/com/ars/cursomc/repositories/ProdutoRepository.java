3a7ee69703bf582974b96b4f1f2955a462bf521e
Angelo Rafael <silva.rafael91k@gmail.com>
2018-12-19 22:55:56 -0200

    Produto e associacao muitos para muitos

---
+++ src/main/java/com/ars/cursomc/repositories/ProdutoRepository.java
@@ -0,0 +1,10 @@
+package com.ars.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ars.cursomc.domain.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+}
