bc5808f64308055ae8d21ff8891ab066610b95eb
Angelo Rafael <silva.rafael91k@gmail.com>
2019-01-06 21:36:00 -0200

    Atualizacao: utilizando somente JsonIgnore

--- src/main/java/com/ars/cursomc/domain/Pedido.java
+++ src/main/java/com/ars/cursomc/domain/Pedido.java
@@ -16,7 +16,6 @@ import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Entity
 public class Pedido implements Serializable {	
@@ -26,11 +25,9 @@ public class Pedido implements Serializable {
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	@JsonFormat(pattern="dd/MM/yyyy HH:mm")
-	private Date instante;	
-	@JsonManagedReference
+	private Date instante;
 	@OneToOne(cascade=CascadeType.ALL, mappedBy="pedido")	
-	private Pagamento pagamento;
-	@JsonManagedReference
+	private Pagamento pagamento;	
 	@ManyToOne
 	@JoinColumn(name="cliente_id")	
 	private Cliente cliente;
