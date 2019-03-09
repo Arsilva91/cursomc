d6f7621f80f61bd3e0fb85451e443de256e74c29
Ângelo Rafael da Silva <silva.rafael91k@gmail.com>
2019-03-09 15:59:24 -0300

    Subtotal e total do pedido

--- src/main/java/com/ars/cursomc/domain/Pedido.java
+++ src/main/java/com/ars/cursomc/domain/Pedido.java
@@ -46,6 +46,13 @@ public class Pedido implements Serializable {
 		this.cliente = cliente;
 		this.enderecoDeEntrega = enderecoDeEntrega;
 	}
+	public double getValorTota() {
+		double soma = 0;
+		for (ItemPedido itemPedido : itens) {
+			soma = soma + itemPedido.getSubTotal();
+		}
+		return soma;
+	}
 	public Integer getId() {
 		return id;
 	}
